3a530d7e3e70f4e63b624eb28cb990206a6ab3c4
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-01 17:18:26 +0100

    step1

---
+++ js/main.js
@@ -0,0 +1,18 @@
+    const emails = ["fabio@gmail.com", "paolo@gmail.com", "marco@gmail.com", "abdu@gmail.com"]
+    
+    const checkBtm = document.getElementById("check")
+
+    checkBtm = addEventListener("click", function())
+    
+    alert = emails.value;
+
+    
+
+    //for (let i = 0; i < emails.length; i++) {
+    //const mail = emails [i]; 
+    //if (mail%3 == 0) {
+    //    console.log(mail);
+    //}
+    
+    
+//}
